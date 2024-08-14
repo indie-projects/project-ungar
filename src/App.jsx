e4b26083d790b967d451b577c50586c659993345
Oliver Spörl <oliver@Hac3r>
2024-08-14 17:02:16 +0200

    products images now centered

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ function App() {
 
   return (
     <>
-    {/* <Products jsonFilePath="/data/output.json" /> */}
-    <EmailJs/>
+    <Products jsonFilePath="/data/output.json" />
+    {/* <EmailJs/> */}
     </>
   )
 }
