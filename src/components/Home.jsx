15d0a3fed4c35c31a2e73d1149fea881c8a05700
dci-student <hugon1992@gmail.com>
2024-08-14 16:56:36 +0200

    Background added-not styled

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,9 @@ const HomeContainer = styled.div`
   padding: 20px;
   text-align: center;
   color: #e0e0e0;
+  background: url("/images/IMG-20240802-WA0004.jpg") no-repeat center center;
+  width: 100vw;
+  height: 100vh;
 `;
 
 const Home = ({ currentLanguage }) => {
